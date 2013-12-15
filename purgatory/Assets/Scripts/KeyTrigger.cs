5b83e119c6ba791a06b4fe7d1ed211e7957470a4
Darren Basler <dbasler1@hotmail.com>
2013-12-15 13:37:21 -0800

    Player plays audio on getting key

--- purgatory/Assets/Scripts/KeyTrigger.cs
+++ purgatory/Assets/Scripts/KeyTrigger.cs
@@ -4,15 +4,20 @@ using System.Collections;
 //[RequireComponent(typeof(AudioSource))]
 public class KeyTrigger : MonoBehaviour {
 	private Inventory inventory;
+	private GameObject player;
 	// Use this for initialization
 	void Start () {
-		inventory = GameObject.Find("Player").GetComponent<Inventory>();
+		player = GameObject.Find("Player");
+		inventory = player.GetComponent<Inventory>();
 		inventory.setKey(false);
+
 	}
 
 	void OnTriggerEnter(Collider other) {
+
 		if (other.gameObject.name=="Player") {
 			inventory.setKey(true);
+			player.audio.Play();
 			Destroy(gameObject);
 		}
 	}
