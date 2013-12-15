dc3819a834b64880bc9653b5526f9bc83f711b7e
Darren Basler <dbasler1@hotmail.com>
2013-12-14 17:48:20 -0800

    Got trigger detection with the player working
    Now the player can enter and exit the trigger and it will only affect
    the player and nothing else.

---
+++ purgatory/Assets/TrapTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name=="Player") {
+			Debug.Log("Entered trigger");
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.name=="Player") {
+			Debug.Log("Exited trigger");
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		//Debug.Log("In trigger");
+	}
+}
