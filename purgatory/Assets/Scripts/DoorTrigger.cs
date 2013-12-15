43b19198d5b60b5db47d6b3bdec8d56e1f5954c6
Darren Basler <dbasler1@hotmail.com>
2013-12-15 00:17:33 -0800

    Finished Door Trigger
    The door trigger will load the next level, but only if you have the key.

---
+++ purgatory/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+	private Inventory inventory;
+	// Use this for initialization
+	void Start () {
+		inventory = GameObject.Find("Player").GetComponent<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name=="Player") {
+			if (inventory.getKey()) {
+				inventory.setKey(false);
+				Application.LoadLevel("level2");
+			}
+		}
+	}
+}
