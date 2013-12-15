b7f008222ee579d307eaf069bf165938227a2ecf
Darren Basler <dbasler1@hotmail.com>
2013-12-14 20:14:09 -0800

    Got player movement working and sharing data WIP
    Player movement works and sharing of data between scripts is going to
    happen soon.

---
+++ purgatory/Assets/Scripts/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+	private bool key;
+	// Use this for initialization
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (getKey()) {
+			Debug.Log("You have the key");
+		}
+	}
+
+	public bool getKey() {
+		return key;
+	}
+
+	public void setKey(bool key) {
+		this.key = key;
+	}
+}
---
+++ purgatory/Assets/Scripts/KeyTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyTrigger : MonoBehaviour {
+	private Inventory inventory;
+	// Use this for initialization
+	void Start () {
+	 	//Inventory inventory1 = (Inventory)gameObject.GetComponent<Inventory>();
+
+		//inventory1.setKey(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name=="Player") {
+			//inventory.setKey(true);
+			Destroy(gameObject);
+		}
+	}
+}
