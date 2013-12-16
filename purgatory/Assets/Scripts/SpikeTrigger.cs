3d00e546f8dfef47053ec8f72ef477cca3f1f690
Darren Basler <dbasler1@hotmail.com>
2013-12-16 14:44:13 -0800

    Spikes Trigger modification
    Trying to get spikes to raise/lower by coroutines.

--- purgatory/Assets/Scripts/SpikeTrigger.cs
+++ purgatory/Assets/Scripts/SpikeTrigger.cs
@@ -5,13 +5,22 @@ public class SpikeTrigger : MonoBehaviour {
 
 	public int state = 1;
 	private PlayerState playerHealth;
+	private GameObject spikes;
 
 	// Use this for initialization
 	void Start () {
 		playerHealth = GameObject.Find("Player").GetComponent<PlayerState>();
+		spikes = GameObject.Find("Spikes");
+	}
+
+	void Update() {
+		StartCoroutine(RaiseSpikes(1.0F));
+		StartCoroutine(LowerSpikes(1.0F));
 	}
 
 	void OnTriggerStay(Collider other) {
+		state = 3;
+		print (state ==3 && other.gameObject.name == "Player");
 		if (state == 3 && other.gameObject.name == "Player") 
 			if (playerHealth.alive == true)
 				StartCoroutine(playerHealth.KillPlayer());
@@ -19,12 +28,19 @@ public class SpikeTrigger : MonoBehaviour {
 	
 	public IEnumerator RaiseSpikes(float delay) {
 		state = 2; //Animate spike position
-		yield return new WaitForSeconds(delay);
+		spikes.audio.Play();
+		yield return new WaitForSeconds (spikes.audio.clip.length);
+
+		//yield return new WaitForSeconds(delay);
 		state = 3; //Animate spike position
-		yield return null;
+
+		//yield return null;
 	}
 
-	public void LowerSpikes() {
+	public IEnumerator LowerSpikes(float delay) {
 		state = 1; //Animate spike position
+		spikes.audio.Play();
+		yield return new WaitForSeconds (spikes.audio.clip.length);
+		//yield return new WaitForSeconds(delay);
 	}
 }
